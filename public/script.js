f8568e468363e315d115ea976d35f95703ae1855
Taptic <taptic@Taptics-MacBook-Pro.local>
2019-12-15 21:24:45 +0330

    first

--- public/script.js
+++ public/script.js
@@ -1,1 +1,21 @@
-console.log("It works!");
+new Vue({
+	el:'#app',
+	data:{
+		total:0,
+		products:[
+			{title:'product 1',id:1},
+			{title:'product 2',id:2},
+			{title:'product 3',id:3},
+
+		],
+		cart:[
+
+		]
+	},
+	methods:{
+		addToCart:function(product){
+			//this.total += 9.99;
+			console.log(product.id);
+		}
+	}
+});
