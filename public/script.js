0f682e88b357879db74643e8e376b60c4d4f0067
Taptic <taptic@Taptics-MacBook-Pro.local>
2019-12-16 18:24:28 +0330

    five

--- public/script.js
+++ public/script.js
@@ -1,21 +1,51 @@
 new Vue({
-	el:'#app',
-	data:{
-		total:0,
-		products:[
-			{title:'product 1',id:1},
-			{title:'product 2',id:2},
-			{title:'product 3',id:3},
-
+	el: "#app",
+	data: {
+		total: 0,
+		products: [
+			{ title: "product 1", id: 1, price: 9.99 },
+			{ title: "product 2", id: 2, price: 9.99 },
+			{ title: "product 3", id: 3, price: 9.99 }
 		],
-		cart:[
-
-		]
+		cart: []
 	},
-	methods:{
-		addToCart:function(product){
-			//this.total += 9.99;
-			console.log(product.id);
+	methods: {
+		addToCart: function(product) {
+			this.total += product.price;
+			var found = false;
+			for (var i = 0; i < this.cart.length; i++) {
+				if (this.cart[i].id === product.id) {
+					this.cart[i].qty++;
+					found = true;
+				}
+			}
+			if (!found) {
+				this.cart.push({
+					id: product.id,
+					price: product.price,
+					title: product.title,
+					qty: 1
+				});
+			}
+		},
+		inc:function(item){
+			 item.qty++;
+			 this.total+=item.price
+			 
+		},
+		dec:function(item){
+			item.qty--;
+			this.total-=item.price;
+			if(item.qty<=0){
+				var i = this.cart.indexOf(item);
+				this.cart.splice(i,1);
+			}
+
+		}
+	},
+	filters: {
+		currency:function(price){
+      return "$".concat(price.toFixed(2));
 		}
 	}
 });
