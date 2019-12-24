c502fbd65516182f5be3f955d21b22d93a8b41fa
Taptic <taptic@Taptics-MacBook-Pro.local>
2019-12-24 20:13:55 +0330

    video-9

--- public/script.js
+++ public/script.js
@@ -1,89 +1,93 @@
 var LOAD_NUM = 4;
 var watcher;
-new Vue({
-	el: "#app",
-	data: {
-		total: 0,
-		products: [],
-		cart: [],
-		search: "",
-		lastSearch: "",
-		loading: false,
-		results: []
-	},
-	methods: {
-		addToCart: function(product) {
-			this.total += product.price;
-			var found = false;
-			for (var i = 0; i < this.cart.length; i++) {
-				if (this.cart[i].id === product.id) {
-					this.cart[i].qty++;
-					found = true;
+//setTimeout(function(){
+	new Vue({
+		el: "#app",
+		data: {
+			total: 0,
+			products: [],
+			cart: [],
+			search: "",
+			lastSearch: "",
+			loading: false,
+			results: []
+		},
+		methods: {
+			addToCart: function(product) {
+				this.total += product.price;
+				var found = false;
+				for (var i = 0; i < this.cart.length; i++) {
+					if (this.cart[i].id === product.id) {
+						this.cart[i].qty++;
+						found = true;
+					}
+				}
+				if (!found) {
+					this.cart.push({
+						id: product.id,
+						price: product.price,
+						title: product.title,
+						qty: 1
+					});
+				}
+			},
+			inc: function(item) {
+				item.qty++;
+				this.total += item.price;
+			},
+			dec: function(item) {
+				item.qty--;
+				this.total -= item.price;
+				if (item.qty <= 0) {
+					var i = this.cart.indexOf(item);
+					this.cart.splice(i, 1);
+				}
+			},
+			onSubmit: function() {
+				this.products = [];
+				this.results = [];
+				this.loading = true;
+				var path = "/search?q=".concat(this.search);
+				this.$http.get(path).then(function(response) {
+					//	setTimeout(function(){
+					this.results = response.body;
+					// this.products = response.body.slice(0, LOAD_NUM);
+					this.lastSearch = this.search;
+					this.appendResults();
+					this.loading = false;
+					//}.bind(this),3000);
+				});
+			},
+			appendResults: function() {
+				if (this.products.length < this.results.length) {
+					var toAppend = this.results.slice(
+						this.products.length,
+						LOAD_NUM + this.products.length
+					);
+					this.products = this.products.concat(toAppend);
 				}
 			}
-			if (!found) {
-				this.cart.push({
-					id: product.id,
-					price: product.price,
-					title: product.title,
-					qty: 1
-				});
+		},
+		filters: {
+			currency: function(price) {
+				return "$".concat(price.toFixed(2));
 			}
 		},
-		inc: function(item) {
-			item.qty++;
-			this.total += item.price;
+		// created:function(){
+		// 	this.onSubmit();
+		// },
+		updated: function() {
+			var sensor = document.querySelector("#product-list-bottom");
+			watcher = scrollMonitor.create(sensor);
+			watcher.enterViewport(this.appendResults);
 		},
-		dec: function(item) {
-			item.qty--;
-			this.total -= item.price;
-			if (item.qty <= 0) {
-				var i = this.cart.indexOf(item);
-				this.cart.splice(i, 1);
-			}
-		},
-		onSubmit: function() {
-			this.products = [];
-			this.results = [];
-			this.loading = true;
-			var path = "/search?q=".concat(this.search);
-			this.$http.get(path).then(function(response) {
-				//	setTimeout(function(){
-				this.results = response.body;
-				// this.products = response.body.slice(0, LOAD_NUM);
-				this.lastSearch = this.search;
-				this.appendResults();
-				this.loading = false;
-				//}.bind(this),3000);
-			});
-		},
-		appendResults: function() {
-			if (this.products.length < this.results.length) {
-				var toAppend = this.results.slice(
-					this.products.length,
-					LOAD_NUM + this.products.length
-				);
-				this.products = this.products.concat(toAppend);
+		beforeUpdate: function() {
+			if (watcher) {
+				watcher.destroy();
+				watcher = null;
 			}
 		}
-	},
-	filters: {
-		currency: function(price) {
-			return "$".concat(price.toFixed(2));
-		}
-	},
-	// created:function(){
-	// 	this.onSubmit();
-	// },
-	updated: function() {
-		var sensor = document.querySelector("#product-list-bottom");
-		watcher = scrollMonitor.create(sensor);
-		watcher.enterViewport(this.appendResults);
-	},
-	beforeUpdate: function() {
-		if (watcher) {
-			watcher.destroy();
-			watcher = null;
-		}
-	}
-});
+	})
+//},3000);
+	
+
